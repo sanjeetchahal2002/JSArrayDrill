cf02e1e8f518226527217cd7227a9350b7c493cf
Sanjeet <Sanjeetsinghchahal933@gmail.com>
2023-09-04 14:53:03 +0530

    Changes in reduce.js

--- reduce.js
+++ reduce.js
@@ -1,9 +1,11 @@
 const items = [1, 2, 3, 4, 5, 5]; 
 
 function reduce(elements, cb, startingValue) {
-    let stVal = 0
+    let stVal;
     if(startingValue != undefined){
         stVal = startingValue
+    }else{
+        stVal= elements[0]
     }
     for(let index in elements){
         stVal = cb(stVal,elements[index])
